55bb3e92eb25104c5200ec7445fe66e474bf5dbc
Eduardo Portugal <epsr@aluno.ifnmg.edu.br>
2025-09-18 17:05:43 +0000

    dudu o rei dos príncipes

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
+import pkg from "pg";
+import dotenv from "dotenv";
 import express from "express";      // Requisição do pacote do express
 const app = express();              // Instancia o Express
 const port = 3000;                  // Define a porta
 
-app.get("/", (req, res) => {        // Cria endpoint na rota da raiz do projeto
+dotenv.config();         // Carrega e processa o arquivo .env
+const { Pool } = pkg;    // Utiliza a Classe Pool do Postgres
+
+app.get("/", async (req, res) => {        // Cria endpoint na rota da raiz do projeto
   console.log("Rota GET / solicitada");
+
+  const db = new Pool({
+    connectionString: process.env.URL_BD,
+  });
+
+  let dbStatus = "ok";
+  try {
+    await db.query("SELECT 1");
+  } catch (e) {
+    dbStatus = e.message;
+  }
+
   res.json({
-		message: "API para atividade",      // Substitua pelo conteúdo da sua API
+    message: "API para atividade",      // Substitua pelo conteúdo da sua API
     author: "Eduardo Portugal Souza Rocha",    // Substitua pelo seu nome
+    statusBD: dbStatus
   });
 });
 
