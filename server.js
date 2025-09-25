dc9ff393570c9d7b5b7841f32b46127d9dffa695
Eduardo Portugal <epsr@aluno.ifnmg.edu.br>
2025-09-25 16:44:29 +0000

    a

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ import express from "express";      // Requisição do pacote do express
 const app = express();              // Instancia o Express
 const port = 3000;                  // Define a porta
 
-dotenv.config();         // Carrega e processa o arquivo .env
-const { Pool } = pkg;    // Utiliza a Classe Pool do Postgres
+dotenv.config();        // Carrega e processa o arquivo .env
+const { Pool } = pkg;   // Utiliza a Classe Pool do Postgres
 
-app.get("/", async (req, res) => {        // Cria endpoint na rota da raiz do projeto
+app.get("/", async (req, res) => {      // Cria endpoint na rota da raiz do projeto
   console.log("Rota GET / solicitada");
 
   const db = new Pool({
@@ -23,10 +23,34 @@ app.get("/", async (req, res) => {        // Cria endpoint na rota da raiz do pr
 
   res.json({
     message: "API para atividade",      // Substitua pelo conteúdo da sua API
-    author: "Eduardo Portugal Souza Rocha",    // Substitua pelo seu nome
+    author: "Eduardo Portugal Souza Rocha",     // Substitua pelo seu nome
     statusBD: dbStatus
   });
 });
+
+// Rota para retornar todas as questões cadastradas
+app.get("/questoes", async (req, res) => {
+	console.log("Rota GET /questoes solicitada"); // Log no terminal para indicar que a rota foi acessada
+
+  // Obtém o construtor Pool do pacote pg para gerenciar conexões com o banco de dados PostgreSQL
+  const db = new Pool({
+    // Cria uma nova instância do Pool para gerenciar conexões com o banco de dados
+    connectionString: process.env.URL_BD, // Usa a variável de ambiente do arquivo .env para a string de conexão
+  });
+
+  try {
+    const resultado = await db.query("SELECT * FROM questoes"); // Executa uma consulta SQL para selecionar todas as questões
+    const dados = resultado.rows; // Obtém as linhas retornadas pela consulta
+    res.json(dados); // Retorna o resultado da consulta como JSON
+  } catch (e) {
+    console.error("Erro ao buscar questões:", e); // Log do erro no servidor
+    res.status(500).json({
+      erro: "Erro interno do servidor",
+      mensagem: "Não foi possível buscar as questões",
+    });
+  }
+});
+
 
 app.listen(port, () => {            // Um socket para "escutar" as requisições
   console.log(`Serviço rodando na porta:  ${port}`);
